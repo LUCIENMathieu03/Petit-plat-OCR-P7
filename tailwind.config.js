4047c0fd01b44c67a22548d9e28cd23fbe52565a
Lucien Mathieu <lucien.mathieu972@gmail.com>
2023-09-07 01:00:35 +0200

    init - starting the project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
